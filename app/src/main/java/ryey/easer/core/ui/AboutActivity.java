0580aa7b19c150eefa50658156d3b6e9951e0e12
AlaskaLinuxUser <alaskalinuxuser@gmail.com>
2017-04-10 10:24:48 -0800

    Update AboutActivity.java
    Added code to make links open in browser for github, website, and license.

--- app/src/main/java/ryey/easer/core/ui/AboutActivity.java
+++ app/src/main/java/ryey/easer/core/ui/AboutActivity.java
@@ -20,8 +20,11 @@
 package ryey.easer.core.ui;
 
 import android.app.Activity;
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
-import android.webkit.WebView;
+import android.support.design.widget.FloatingActionButton;
+import android.view.View;
 
 import ryey.easer.R;
 
@@ -31,7 +34,70 @@ public class AboutActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_about);
-        WebView wv = (WebView) findViewById(R.id.webView_about);
-        wv.loadData(getString(R.string.about), "text/html", "utf-8");
+
+        FloatingActionButton fabGit = (FloatingActionButton) findViewById(R.id.fabGithub);
+        fabGit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                myHubGit();
+
+            }
+        });
+
+        FloatingActionButton fabWord = (FloatingActionButton) findViewById(R.id.fabWord);
+        fabWord.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                myWebsite();
+
+            }
+        });
+
+        FloatingActionButton fabBack = (FloatingActionButton) findViewById(R.id.fabReturn);
+        fabBack.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                goBack();
+
+            }
+        });
     }
+
+    // When they click on the github icon.
+    public void myHubGit () {
+
+        goToUrl ( "https://github.com/renyuneyun/Easer");
+
+    }
+
+    // When they click on the wordpress icon.
+    public void myWebsite () {
+
+        goToUrl ( "http://renyuneyun.is-programmer.com/");
+
+    }
+
+    // When they click on the license.
+    public void howL (View view) {
+
+        goToUrl ( "https://www.gnu.org/licenses/gpl.txt");
+
+    }
+
+    // To launch one of the above URL's.
+    private void goToUrl (String url) {
+        Uri uriUrl = Uri.parse(url);
+        Intent launchBrowser = new Intent(Intent.ACTION_VIEW, uriUrl);
+        startActivity(launchBrowser);
+    }
+
+    // To kill this activity and go back to Activity Main.
+    public void goBack () {
+        finish();
+    }
+
+
 }
