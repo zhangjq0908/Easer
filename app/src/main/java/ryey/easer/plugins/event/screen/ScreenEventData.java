6c30a93a41e51ba487bb69007a78cefb61957499
Rui Zhao (renyuneyun) <renyuneyun@gmail.com>
2018-04-26 01:01:38 +0100

    Add ScreenEventPlugin - Listen to screen on/off
    Close #36

---
+++ app/src/main/java/ryey/easer/plugins/event/screen/ScreenEventData.java
@@ -0,0 +1,99 @@
+/*
+ * Copyright (c) 2016 - 2018 Rui Zhao <renyuneyun@gmail.com>
+ *
+ * This file is part of Easer.
+ *
+ * Easer is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Easer is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Easer.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package ryey.easer.plugins.event.screen;
+
+import android.os.Parcel;
+import android.support.annotation.NonNull;
+
+import ryey.easer.commons.C;
+import ryey.easer.commons.IllegalStorageDataException;
+import ryey.easer.plugins.event.AbstractEventData;
+
+public class ScreenEventData extends AbstractEventData {
+
+    enum ScreenEvent {
+        on,
+        off,
+    }
+
+    final ScreenEvent screenEvent;
+
+    ScreenEventData(ScreenEvent screenEvent) {
+        this.screenEvent = screenEvent;
+    }
+
+    ScreenEventData(@NonNull String data, @NonNull C.Format format, int version) throws IllegalStorageDataException {
+        screenEvent = ScreenEvent.valueOf(data);
+    }
+
+    @NonNull
+    @Override
+    public String serialize(@NonNull C.Format format) {
+        String res;
+        switch (format) {
+            default:
+                res = screenEvent.name();
+        }
+        return res;
+    }
+
+    @SuppressWarnings({"SimplifiableIfStatement", "RedundantIfStatement"})
+    @Override
+    public boolean isValid() {
+        if (screenEvent == null)
+            return false;
+        return true;
+    }
+
+    @SuppressWarnings({"SimplifiableIfStatement", "RedundantIfStatement"})
+    @Override
+    public boolean equals(Object obj) {
+        if (obj == null || !(obj instanceof ScreenEventData))
+            return false;
+        if (!screenEvent.equals(((ScreenEventData) obj).screenEvent))
+            return false;
+        return true;
+    }
+
+    @Override
+    public int describeContents() {
+        return 0;
+    }
+
+    @Override
+    public void writeToParcel(Parcel dest, int flags) {
+        screenEvent.ordinal();
+    }
+
+    public static final Creator<ScreenEventData> CREATOR
+            = new Creator<ScreenEventData>() {
+        public ScreenEventData createFromParcel(Parcel in) {
+            return new ScreenEventData(in);
+        }
+
+        public ScreenEventData[] newArray(int size) {
+            return new ScreenEventData[size];
+        }
+    };
+
+    private ScreenEventData(Parcel in) {
+        screenEvent = ScreenEvent.values()[in.readInt()];
+    }
+}
---
+++ app/src/main/java/ryey/easer/plugins/event/screen/ScreenEventPlugin.java
@@ -0,0 +1,82 @@
+/*
+ * Copyright (c) 2016 - 2018 Rui Zhao <renyuneyun@gmail.com>
+ *
+ * This file is part of Easer.
+ *
+ * Easer is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Easer is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Easer.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package ryey.easer.plugins.event.screen;
+
+import android.app.Activity;
+import android.content.Context;
+import android.support.annotation.NonNull;
+
+import ryey.easer.R;
+import ryey.easer.commons.plugindef.PluginViewFragment;
+import ryey.easer.commons.plugindef.ValidData;
+import ryey.easer.commons.plugindef.eventplugin.AbstractSlot;
+import ryey.easer.commons.plugindef.eventplugin.EventDataFactory;
+import ryey.easer.commons.plugindef.eventplugin.EventPlugin;
+
+public class ScreenEventPlugin implements EventPlugin<ScreenEventData> {
+
+    @NonNull
+    @Override
+    public String id() {
+        return "screen";
+    }
+
+    @Override
+    public int name() {
+        return R.string.event_screen;
+    }
+
+    @Override
+    public boolean isCompatible(@NonNull final Context context) {
+        return true;
+    }
+
+    @Override
+    public boolean checkPermissions(@NonNull Context context) {
+        return true;
+    }
+
+    @Override
+    public void requestPermissions(@NonNull Activity activity, int requestCode) {
+    }
+
+    @NonNull
+    @Override
+    public EventDataFactory<ScreenEventData> dataFactory() {
+        return new ScreenEventDataFactory();
+    }
+
+    @NonNull
+    @Override
+    public PluginViewFragment<ScreenEventData> view() {
+        return new ScreenPluginViewFragment();
+    }
+
+    @Override
+    public AbstractSlot<ScreenEventData> slot(@NonNull Context context, @ValidData @NonNull ScreenEventData data) {
+        return new ScreenSlot(context, data);
+    }
+
+    @Override
+    public AbstractSlot<ScreenEventData> slot(@NonNull Context context, @NonNull ScreenEventData data, boolean retriggerable, boolean persistent) {
+        return new ScreenSlot(context, data, retriggerable, persistent);
+    }
+
+}
