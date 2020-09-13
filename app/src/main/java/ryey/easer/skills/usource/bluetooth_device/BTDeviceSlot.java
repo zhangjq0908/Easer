799d180df0d10cca769c64d9254b9d186d80f987
Uladzislau Vasilyeu <vasvlad@gmail.com>
2020-09-13 12:55:29 +0300

    Bluethooth match counter error (#332)
    * Fixed error in matched_devices counter. This counter cannot be negative.
    
    Co-authored-by: Uladzislau Vasilyeu <vlad@oblgas.by>

--- app/src/main/java/ryey/easer/skills/usource/bluetooth_device/BTDeviceSlot.java
+++ app/src/main/java/ryey/easer/skills/usource/bluetooth_device/BTDeviceSlot.java
@@ -53,6 +53,8 @@ public class BTDeviceSlot extends AbstractSlot<BTDeviceUSourceData> {
                 BluetoothDevice device = intent.getParcelableExtra(BluetoothDevice.EXTRA_DEVICE);
                 if (is_target(device)) {
                     matched_devices--;
+                    if (matched_devices<0)
+                        matched_devices = 0;
                     determine_satisfied(dynamicsForCurrentDevice(device));
                 }
             }
