791b87f16f7be26a927256bd9fc5ede6cb8a8a5f
renyuneyun (Rui Zhao) <renyuneyun@gmail.com>
2021-09-11 22:36:27 +0100

    Support Dynamics in SMS operation's destination
    Close #382

--- app/src/main/java/ryey/easer/skills/operation/send_sms/SmsLoader.java
+++ app/src/main/java/ryey/easer/skills/operation/send_sms/SmsLoader.java
@@ -34,8 +34,8 @@ public class SmsLoader extends OperationLoader<SmsOperationData> {
 
     @Override
     public void _load(@ValidData @NonNull SmsOperationData data, @NonNull OnResultCallback callback) {
-        String destination = data.destination;
-        String content = data.content;
+        String destination = data.destination.toString();
+        String content = data.content.toString();
         SmsManager smsManager = SmsManager.getDefault();
         if (smsManager == null) {
             callback.onResult(false);
